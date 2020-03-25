043cb21cd280baf122f7c1c3117043eb198a98d9
BaMarkham1 <benjamin.lemarc@ucdenver.edu>
2020-03-25 12:30:45 -0600

    commit 1

---
+++ weather_service.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 11:15:12 2020
+
+@author: lemar
+"""
+import json
+import weather_grabber as wg
+
+#set the zip code you want here
+zip_code = "80215"
+#set the type of forecast you want
+#valid types: currently, daily, hourly, and alerts
+forecast_type = "daily"
+#call the weather_grabber module's get_weather function
+data = wg.get_weather(zip_code, forecast_type)
+#this is a default way to print a json in an organized fashion
+#this can be changed to send a message to the user in the way we want
+print(json.dumps(data, indent=2))
+
